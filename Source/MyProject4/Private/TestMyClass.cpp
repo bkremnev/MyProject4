8c3f53ce2372fc6be1656d17be7b19e289a5c333
bkremnev <bogdan.kremnev@gmail.com>
2015-07-06 11:01:06 +0300

    Test Commit
    Added TestMyClass

---
+++ Source/MyProject4/Private/TestMyClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyProject4.h"
+#include "TestMyClass.h"
+
+TestMyClass::TestMyClass()
+{
+}
+
+TestMyClass::~TestMyClass()
+{
+}
---
+++ Source/MyProject4/Public/TestMyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class MYPROJECT4_API TestMyClass
+{
+public:
+	TestMyClass();
+	~TestMyClass();
+};
